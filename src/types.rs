c1a517a16255079a477441d4586054e1934a36f3
byt3-m3 <cbaxtertech@gmail.com>
2022-02-10 21:25:00 -0500

    Added primitive type basics

--- src/types.rs
+++ src/types.rs
@@ -13,5 +13,30 @@ Rust is statitcally typed, but its not required to set the type during variable 
 
 
 pub fn run() {
+    // Default is "i32"
+    let x = 1;
+
+    // Default is "f64"
+    let y = 2.5;
+
+    // Explicit Type
+    let z: i64 = 123456789123456789;
+
+    // Find max size
+    println!("Maix i32: {}", std::i32::MAX);
+    println!("Maix i64: {}", std::i64::MAX);
+    
+    // Boolean 
+    let is_actve = true;
+    let is_not_actve: bool = false;
+
+
+    // Get boolean from experession 
+    let is_greater =  10 > 5;
+
+    // Char - Chars use single quotes oppsoed to double quotes, has to be 1 character 
+    let a1 = 'a';
+    let face = '\u{1F600}'; // Emoji smilling face
+    println!("{:?}", (x, y, z, is_actve, is_not_actve, is_greater, a1, face))
 
 }
